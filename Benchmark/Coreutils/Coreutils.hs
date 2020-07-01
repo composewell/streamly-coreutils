864b2162086591fa2e6a4dad79c763a74882550e
Shruti Umat <shruti.umat@composewell.com>
2020-07-01 21:02:13 +0530

    Add bench for cat

--- Benchmark/Coreutils/Coreutils.hs
+++ Benchmark/Coreutils/Coreutils.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Streamly.Coreutils
 import qualified Streamly.Coreutils.Cp as C
+import qualified Streamly.Coreutils.Cat as Cat
 import qualified Streamly.Coreutils.Uniq as U
 
 import qualified Streamly.Prelude as S
@@ -17,9 +18,11 @@ import Streamly.Internal.Data.Stream.Serial (SerialT(..))
 import Streamly.Internal.Data.Stream.StreamK.Type (IsStream)
 
 import Data.Word (Word8)
+import GHC.IO.Handle.FD (openFile)
 import System.Environment (getArgs)
 import Control.Monad.IO.Class (MonadIO)
 import System.IO (Handle, stdout, hPutStr)
+import System.IO (stdout, IOMode(ReadMode))
 import Streamly.Data.Unicode.Stream (decodeLatin1)
 
 import Gauge (defaultMain, benchmark, Benchmarkable (..))
@@ -57,6 +60,14 @@ arrayChar fp = U.splitOnNewLine
              $ U.ignoreCase False
              $ File.toBytes fp
 
+intStrStrm :: (IsStream t, Monad m, MonadCatch m, MonadIO m) => Int -> FilePath -> t m (Int, String)
+intStrStrm n fp = U.uniqCount n
+                $ U.splitOnNewLine
+                $ Un.decodeLatin1
+                $ File.toBytes fp
+
+handleStrm :: IsStream t => [FilePath] -> t IO Handle
+handleStrm fpl = S.mapM (\s -> openFile s ReadMode) $ S.fromList fpl
 
 main :: IO ()
 main = do
@@ -70,6 +81,14 @@ main = do
                bench "ignoreCase" $ nfAppIO (\strm -> S.drain $ U.ignoreCase True strm) (byteStrm srcFP),
                bench "splitOnNewLine" $ nfAppIO (\strm -> S.drain $ U.splitOnNewLine strm) (charStrm srcFP),
                bench "uniqCount skip 0" $ nfAppIO (\strm -> S.drain $ U.uniqCount 0 strm) (arrayChar "/home/shruti/sorted.txt"),
-               bench "uniqCount skip 1000" $ nfAppIO (\strm -> S.drain $ U.uniqCount 1000 strm) (arrayChar "/home/shruti/sorted.txt")
+               bench "uniqCount skip 1000" $ nfAppIO (\strm -> S.drain $ U.uniqCount 1000 strm) (arrayChar "/home/shruti/sorted.txt"),
+               bench "uniqRepeated skip 0" $ nfAppIO (\strm -> S.drain $ U.uniqRepeated strm) (intStrStrm 0 "/home/shruti/sorted.txt"),
+               bench "uniqRepeated skip 1000" $ nfAppIO (\strm -> S.drain $ U.uniqRepeated strm) (intStrStrm 1000 "/home/shruti/sorted.txt"),
+               bench "uniqDistinct skip 0" $ nfAppIO (\strm -> S.drain $ U.uniqDistinct strm) (intStrStrm 0 "/home/shruti/sorted.txt"),
+               bench "uniqDistinct skip 1000" $ nfAppIO (\strm -> S.drain $ U.uniqDistinct strm) (intStrStrm 1000 "/home/shruti/sorted.txt"),
+               bench "uniqDistinct skip 1000 - nfIO" $ nfIO (S.drain $ U.uniqDistinct $ intStrStrm 1000 "/home/shruti/sorted.txt")
+            ],
+            bgroup "cat" [
+               bench "cat" $ nfIO (S.drain $ Cat.cat Cat.defaultCatOptions stdout (handleStrm [srcFP]))
             ]
           ]
