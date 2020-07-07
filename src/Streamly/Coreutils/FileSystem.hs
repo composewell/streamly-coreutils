fed8e4bfc1d2c6208a66056c9ef1592d1c8db5e8
Shruti Umat <shruti.umat@composewell.com>
2020-07-07 13:10:43 +0530

    Filesystem based utilities

---
+++ src/Streamly/Coreutils/FileSystem.hs
@@ -0,0 +1,58 @@
+module Streamly.Coreutils.FileSystem (
+      defaultRmdirOptions
+    , RmdirOptions (..)
+    ,
+    ,
+    ,
+    ,
+    ,
+   )
+where
+import Control.Monad.Trans.Control (MonadBaseControl (..))
+
+import Streamly.Coreutils.Common
+import qualified Streamly.Prelude as S
+import qualified Streamly.Memory.Array as A
+import qualified Streamly.Internal.Data.Fold as FL
+import qualified Streamly.Data.Unicode.Stream as Un
+import qualified Streamly.Internal.FileSystem.File as File
+import qualified Streamly.Internal.FileSystem.Handle as FH
+
+import Data.Word (Word8)
+import System.IO (hPutStr)
+import Data.Char (toLower, isSpace)
+import System.IO (Handle, stdout)
+import Foreign.Storable (Storable)
+import Control.Monad.Catch (MonadCatch, MonadThrow)
+import System.IO.Unsafe (unsafePerformIO)
+import Streamly.Internal.Data.SVar (MonadAsync)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Streamly.Internal.Data.Stream.Serial(SerialT)
+import Streamly.Internal.Data.Stream.StreamK.Type (IsStream)
+
+-------------------------------------------------------------------------------
+-- Record for options used with uniq
+-------------------------------------------------------------------------------
+
+{-# INLINE intMax #-}
+
+intMax :: Int
+intMax = maxBound
+
+
+data RmdirOptions = UniqOptions {
+                       verbose :: Bool
+                     , ignoreFail :: Bool   -- ignore failure because of a non-empty directory
+                     , parents :: Bool      -- remove directory & its ancestors
+                                            -- rmdir -p a/b/c is similar to rmdir a/b/c a/b a
+                  }
+
+{-# INLINE defaultRmdirOptions #-}
+
+defaultRmdirOptions :: RmdirOptions
+defaultRmdirOptions = RmdirOptions True False False
+
+
+-------------------------------------------------------------------------------
+-- helper functions for rmdir
+-------------------------------------------------------------------------------
