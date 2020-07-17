9c8930245e8f7911e1a493801b4301072be50277
Shruti Umat <shruti.umat@composewell.com>
2020-07-18 01:15:00 +0530

    Generate char stream for tests instead of reading from file
    Cleanup gen function in tests

--- test/Main.hs
+++ test/Main.hs
@@ -2,29 +2,16 @@ module Main(main) where
 
 import qualified Streamly.Prelude as S
 import qualified Streamly.Internal.Data.Fold as FL
-import qualified Streamly.Data.Unicode.Stream as Un
-import qualified Streamly.Internal.FileSystem.File as File
 
 import Streamly.Coreutils.Uniq
 
 import Control.Monad.IO.Class (MonadIO)
-import Control.Monad.Catch (MonadCatch)
-import System.Directory (getHomeDirectory)
 import Streamly.Internal.Data.Stream.StreamK.Type (IsStream)
-
-home :: IO FilePath
-home = getHomeDirectory
 
 opt :: UniqOptions
 opt = defaultUniqOptions { skipFields = 1
                          , skipChar = 1
-                         , ignoreCase = True
                          }
-
-charStrm
-    :: (IsStream t, Monad m, MonadCatch m, MonadIO m)
-    => FilePath -> t m Char
-charStrm = Un.decodeLatin1 . File.toBytes
 
 splitOnNewLine
     :: (IsStream t, Monad m, MonadIO m)
@@ -32,21 +19,36 @@ splitOnNewLine
 splitOnNewLine = S.splitOnSuffix (== '\n') FL.toList
 
 
+gen :: (IsStream t, Monad m) => Char -> Int -> t m Char
+gen c n = S.unfoldr step (0, 1)
+    where
+        step (i, flg) | i == n = Just ('\n', (0, flg))
+        step (0, 0) = Just ('x', (1, 1))
+        step (0, 1) = Just ('y', (1, 0))
+        step (1, flg) = Just (' ', (2, flg))
+        step (i, flg) = Just (c, (i + 1, flg))
+
+
 main :: IO ()
 main = do
-    homeFP <- home
-    let srcFP = homeFP ++ "/test-uniq.txt"
     let comp = compareUsingOptions opt
+    S.drain $ S.mapM print
+        $ splitOnNewLine
+        $ S.take 100
+        $ gen 'a' 6
     S.drain $ S.mapM print
         $ getRepetition comp
         $ splitOnNewLine
-        $ charStrm srcFP
+        $ S.take 100
+        $ gen 'a' 6
     S.drain $ S.mapM print
         $ uniq opt
         $ splitOnNewLine
-        $ charStrm srcFP
+        $ S.take 100
+        $ gen 'a' 6
     S.drain $ S.mapM print
         $ uniqResultToString
         $ uniq opt
         $ splitOnNewLine
-        $ charStrm srcFP
+        $ S.take 100
+        $ gen 'a' 6
