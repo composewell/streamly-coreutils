a9fb669fef9bbfc389b73903b000d212a0d80ba5
Shruti Umat <shruti.umat@composewell.com>
2020-07-28 00:28:56 +0530

    Add dfs function

--- src/Streamly/Coreutils.hs
+++ src/Streamly/Coreutils.hs
@@ -1,6 +1,9 @@
 module Streamly.Coreutils
     ( module Streamly.Coreutils.Tsort
     , module Streamly.Coreutils.Uniq
+    --, module Streamly.Coreutils.Common
     ) where
 
+import Streamly.Coreutils.Tsort
 import Streamly.Coreutils.Uniq
+--import Streamly.Coreutils.Common
--- src/Streamly/Coreutils/Tsort.hs
+++ src/Streamly/Coreutils/Tsort.hs
@@ -1,12 +1,6 @@
 module Streamly.Coreutils.Tsort
-    (
-    ,
-    ,
-    ,
-    ,
-    ,
-    ,
-    ,
+    ( vertices
+    --, dfs
     ) where
 
 import qualified Streamly.Prelude as S
@@ -23,51 +17,81 @@ vertices
     -> t m (Int, a)
     -- ^ Map each vertex to a unique integer
 vertices strm =
-    S.indexed $
-    S.uniq $
-    S.concatMap (\(x, y) -> S.fromList [x, y]) strm
+    S.indexed
+    $ S.uniq
+    $ S.concatMap (\(x, y) -> S.fromList [x, y]) strm
 
 
-buildAdjList
-    :: (IsStream t, Monad m)
-    => t m (a, a)
-    -- ^ stream of edges
-    -> t m (Int, a)
-    -- ^ Map from @a@ to @Int@
-    -> t m (t m Int)
-    -- ^ initial adj list
-    -> t m (t m Int)
-    -- ^ adj list now
+--buildAdjList
+--    :: (IsStream t, Monad m)
+--    => t m (a, a)
+--    -- ^ stream of edges
+--    -> t m (Int, a)
+--    -- ^ Map from @a@ to @Int@
+--    -> t m (t m Int)
+--    -- ^ initial adj list
+--    -> t m (t m Int)
+--    -- ^ adj list now
 
 
 dfs
     :: (IsStream t, Monad m)
     => Int
     -- ^ root node to start dfs
-    -> t m (t m Int)
+    -> SerialT m (SerialT m Int)
     -- ^ adj list
-    -> t m Bool
+    -> SerialT m Bool
     -- ^ visited
     -> t m Int
     -- ^ parent
     -> t m Int
     -- ^ the stack
-    -> m ()
+    -> m (SerialT m Bool, t m Int, t m Int)
+    -- ^ (parent, stack)
 dfs root adj vis par stck = do
-    strm <- S.(!!) adj root
+    strm <- (S.!!) adj root
     case strm of
-        Just nbd -> S.filterM isVisited nbd
-        Nothing -> return ()
+        Just nbd -> do
+            maybeTuple <- S.last
+               $ S.scanlM'
+               (\(visi, parent, stack) v -> dfs v adj
+                  (markVisited visi v) (setParent parent v root) (S.cons v stck))
+               (vis, par, stck)
+               $ S.filterM (unVisited vis) nbd
+            case maybeTuple of
+                   Just ans -> return ans
+        Nothing -> return (vis, par, stck)
 
     where
 
-    isVisited
-        :: (IsStream t, Monad m)
-        -> t m Bool
+    unVisited
+        :: Monad m
+        => SerialT m Bool
         -> Int
         -> m Bool
-    isVisited vis n = do
-        ele <- S.(!!) vis n
+    unVisited vis n = do
+        ele <- (S.!!) vis n
         case ele of
-            Just v -> return v
-            Nothing -> return True
+            Just v -> return $ not v
+            Nothing -> return False
+
+    markVisited
+       :: (IsStream t, Monad m)
+       => t m Bool
+       -> Int
+       -> t m Bool
+    markVisited strm vtx = do
+        S.map (\(_, v) -> v)
+        $ S.map (\(i, v) -> if i == vtx then (i, not v) else (i, v))
+        $ S.indexed strm
+
+    setParent
+        :: (IsStream t, Monad m)
+        => t m Int
+        -> Int
+        -> Int
+        -> t m Int
+    setParent strm u par = do
+        S.map (\(_, v) -> v)
+        $ S.map (\(i, v) -> if i == u then (i, par) else (i, v))
+        $ S.indexed strm
