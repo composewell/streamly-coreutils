5352a9b01b34b9f538f278065d2f5d45a8439e7c
Ranjeet Kumar Ranjan <ranjeetr@composewell.com>
2022-05-02 17:30:11 +0530

    Add Symbolic link module
    Fix merge issues
    
    Fix code refactoring
    
    Move case statement out of when block
    
    Fix review comments

---
+++ src/Streamly/Coreutils/Ln.hs
@@ -0,0 +1,52 @@
+-- |
+-- Module      : Streamly.Coreutils.Ln
+-- Copyright   : (c) 2022 Composewell Technologies
+-- License     : Apache-2.0
+-- Maintainer  : streamly@composewell.com
+-- Stability   : experimental
+-- Portability : GHC
+--
+-- create a link to TARGET with the given name
+
+module Streamly.Coreutils.Ln
+    (
+      Ln
+    , ln
+    , force
+    , symbolic
+    )
+where
+
+import Control.Monad (when)
+import Streamly.Coreutils.Common (Switch(..))
+import Streamly.Coreutils.FileTest (test, isExisting)
+
+import qualified System.Posix.Files as Posix
+
+data Ln = Ln
+    { lnForce :: Switch
+    , lnSymbolic :: Switch
+    }
+
+defaultConfig :: Ln
+defaultConfig = Ln Off Off
+
+force :: Switch -> Ln -> Ln
+force opt cfg = cfg {lnForce = opt}
+
+symbolic :: Switch -> Ln -> Ln
+symbolic opt cfg = cfg {lnSymbolic = opt}
+
+ln :: (Ln -> Ln) -> FilePath -> FilePath -> IO ()
+ln f src tgt = do
+    let opt = f defaultConfig
+    when (lnForce opt == Off) $ do
+        found <- test tgt isExisting
+        when found $ error msg
+    case lnSymbolic opt of
+        Off -> Posix.createLink src tgt
+        On -> Posix.createSymbolicLink src tgt
+
+    where
+
+    msg = "ln: Link target exists, use force to create anyway"
