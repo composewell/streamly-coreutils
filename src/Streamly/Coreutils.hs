a09e856698770bb6ecb8416a0db297f78b77997c
Harendra Kumar <harendra@composewell.com>
2022-06-02 18:00:16 +0530

    Export FileTest and Cp

--- src/Streamly/Coreutils.hs
+++ src/Streamly/Coreutils.hs
@@ -1,5 +1,18 @@
+-- |
+-- Module      : Streamly.Coreutils
+-- Copyright   : (c) 2020 Composewell Technologies
+-- License     : Apache-2.0
+-- Maintainer  : streamly@composewell.com
+-- Stability   : experimental
+-- Portability : GHC
+--
 module Streamly.Coreutils
     ( module Streamly.Coreutils.Uniq
-    ) where
+    , module Streamly.Coreutils.Cp
+    , module Streamly.Coreutils.FileTest
+    )
+where
 
 import Streamly.Coreutils.Uniq
+import Streamly.Coreutils.Cp
+import Streamly.Coreutils.FileTest
