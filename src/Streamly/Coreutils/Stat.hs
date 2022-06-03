01fd173af42802ca4ebd84ee88e16afc146ec4ed
Ranjeet Kumar Ranjan <ranjeetr@composewell.com>
2022-06-03 16:30:29 +0530

    Add Stat module

---
+++ src/Streamly/Coreutils/Stat.hs
@@ -0,0 +1,42 @@
+-- |
+-- Module      : Streamly.Coreutils.Stat
+-- Copyright   : (c) 2022 Composewell Technologies
+-- License     : Apache-2.0
+-- Maintainer  : streamly@composewell.com
+-- Stability   : experimental
+-- Portability : GHC
+--
+-- gets the FileStatus information (user ID,
+-- size, access times, etc.) for the file path.
+
+-- XXX Reexport the required Posix types?
+
+module Streamly.Coreutils.Stat
+    (
+      stat
+
+    -- * Options
+    , Stat
+    , followLinks
+    ) where
+
+
+import Streamly.Coreutils.Common (Switch(..))
+import System.Posix.Files (FileStatus)
+
+import qualified System.Posix.Files as Files
+
+newtype  Stat = Stat {deRef :: Switch}
+
+defaultConfig :: Stat
+defaultConfig = Stat On
+
+followLinks :: Switch -> Stat -> Stat
+followLinks opt cfg = cfg {deRef = opt}
+
+stat :: (Stat -> Stat) -> FilePath -> IO FileStatus
+stat f = do
+    let opt = f defaultConfig
+    case deRef opt of
+        Off -> Files.getSymbolicLinkStatus
+        On -> Files.getFileStatus
