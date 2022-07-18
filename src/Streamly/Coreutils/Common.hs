edf66d29cdd6ae7b89b909f06821d760c9971609
Harendra Kumar <harendra@composewell.com>
2022-07-19 02:35:48 +0530

    Remove dependency on data-default-class

--- src/Streamly/Coreutils/Common.hs
+++ src/Streamly/Coreutils/Common.hs
@@ -2,10 +2,4 @@ module Streamly.Coreutils.Common
     ( Switch (..))
 where
 
-import Data.Default.Class (Default(..))
-
 data Switch = On | Off deriving (Show, Eq)
-
-instance Default Switch
-    where
-    def = Off
